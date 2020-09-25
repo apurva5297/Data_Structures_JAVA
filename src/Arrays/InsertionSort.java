22983ff82567c1adc32dbb2897c3d25aca1c5485
apurva5297 <apurva5297@gmail.com>
2020-09-25 14:58:18 +0530

    Fibonacci

--- src/Arrays/InsertionSort.java
+++ src/Arrays/InsertionSort.java
@@ -4,13 +4,10 @@ public class InsertionSort {
     public static void main(String[] args) {
 
         int[] intArray = { 20, 35, -15, 7, 55, 1, -22 };
-
+        int i;
         for (int firstUnsortedIndex = 1; firstUnsortedIndex < intArray.length;
              firstUnsortedIndex++) {
             int newElement = intArray[firstUnsortedIndex];
-
-            int i;
-
             for (i = firstUnsortedIndex; i > 0 && intArray[i - 1] > newElement; i--) {
                 intArray[i] = intArray[i - 1];
             }
